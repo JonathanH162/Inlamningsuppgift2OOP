28544c10d07271e31905d70abbddbe298384f5d8
Jonathan Hellgren <jonathan.hellgren7@gmail.com>
2022-10-20 18:05:13 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Path customerInfo = Paths.get("src/customers.txt");
+
+        Methods methods = new Methods();
+
+        methods.customerFileIteration(JOptionPane.showInputDialog(null, "Skriv in ett namn eller personnummer:").trim(), customerInfo);
+    }
+
+}
---
+++ src/Methods.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+public class Methods {
+    public void customerFileIteration(String input, Path readFile) {
+        String readFirstLine;
+        String readSecondLine;
+
+        try (Scanner reader = new Scanner(readFile)) {
+            while (reader.hasNext()) {
+                readFirstLine = reader.nextLine();
+                readSecondLine = reader.nextLine();
+
+                String[] firstLineArray = readFirstLine.split(", ");
+
+                if (input == null) {
+                    JOptionPane.showMessageDialog(null,"Du avslutade programmet.");
+                    return;
+                }
+                else if (input.equalsIgnoreCase(firstLineArray[0].trim()) || input.equalsIgnoreCase(firstLineArray[1].trim())) {
+                    compareDate(readFirstLine,readSecondLine);
+                    return;
+                }
+                else if (input.isBlank()) {
+                    JOptionPane.showMessageDialog(null,"Input-raden får ej lämnas tom.");
+                    return;
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Personen du angav är inte medlem.");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void compareDate(String readFirstLine, String readSecondLine) {
+            try {
+                Period timeCompare = Period.between(LocalDate.parse(readSecondLine), LocalDate.now());
+            if (timeCompare.toTotalMonths() < 12 ){
+                JOptionPane.showMessageDialog(null, "Personen du angav är en aktiv medlem och " +
+                                                                        "betalade sin årsavgift för " + timeCompare.toTotalMonths() + " månader sen.");
+                writeToFile(readFirstLine);
+            }
+            else if (timeCompare.toTotalMonths() > 12 ) {
+                JOptionPane.showMessageDialog(null, "Personen du angav är en vilande medlem och " +
+                                                                        "betalade sin senaste årsavgift för " + timeCompare.toTotalMonths() + " månader sen.");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void writeToFile (String readFile) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/gymVisitors.txt", true))) {
+            writer.write("\n" + readFile + ", " + LocalDate.now());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
